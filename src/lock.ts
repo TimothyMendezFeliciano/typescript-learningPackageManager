a1425cf12b0d0e9a282d5df8ebde0cb664c1b01f
TimothyMendezFeliciano <timothy.mendez@upr.edu>
2022-12-28 12:23:06 -0400

    Lock file created

--- src/lock.ts
+++ src/lock.ts
@@ -1,1 +1,58 @@
-export default function lock() {}
+import * as fs from 'fs-extra'
+import * as yaml from 'js-yaml'
+import * as utils from './utils'
+import {Manifest} from './resolve'
+import it from "node:test";
+import * as util from "util";
+
+interface Lock {
+    [index: string]: {
+        url: string,
+        version: string,
+        shasum: string,
+        dependencies: { [dependency: string]: string },
+    }
+}
+
+
+const oldLock: Lock = Object.create(null) // Only for reading
+const newLock: Lock = Object.create(null) // Only for writing
+
+export function updateOrCreate(name: string, info: object) {
+    if (!newLock[name]) {
+        newLock[name] = Object.create(null)
+    }
+
+    Object.assign(newLock[name], info)
+}
+
+export function getItem(name: string, constraints: string): Manifest | null {
+    const item = oldLock[`${name}@${constraints}`]
+
+    if (!item) {
+        return null
+    }
+
+    return {
+        [item.version]: {
+            dependencies: item.dependencies,
+            dist: {shasum: item.shasum, tarball: item.url}
+        }
+    }
+}
+
+export async function writeLock() {
+    await fs.writeFile(
+        './tiny-pm.yml',
+        yaml.safeDump(utils.sortKeys(newLock), {noRefs: true})
+    )
+}
+
+export async function readLock() {
+    if(fs.path('./tiny-pm.yml')) {
+        Object.assign(
+            oldLock,
+            yaml.safeLoad(await fs.readFile('./tiny-pm.yml', 'utf-8'))
+        )
+    }
+}
